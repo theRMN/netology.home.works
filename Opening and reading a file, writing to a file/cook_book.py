e66c93efe0f83dff33af5664660566fde2af5890
theRMN <roman.vassev@gmail.com>
2021-02-24 19:20:40 +0600

    add function: making_cook_book

--- Opening and reading a file, writing to a file/cook_book.py
+++ Opening and reading a file, writing to a file/cook_book.py
@@ -1,28 +1,60 @@
 from pprint import pprint
 
-cook_book = {
-    'Омлет': [
-        {'ingredient_name': 'Яйцо', 'quantity': 2, 'measure': 'шт.'},
-        {'ingredient_name': 'Молоко', 'quantity': 100, 'measure': 'мл'},
-        {'ingredient_name': 'Помидор', 'quantity': 6, 'measure': 'шт'}
-    ],
-    'Утка по-пекински': [
-        {'ingredient_name': 'Утка', 'quantity': 1, 'measure': 'шт'},
-        {'ingredient_name': 'Вода', 'quantity': 2, 'measure': 'л'},
-        {'ingredient_name': 'Мед', 'quantity': 3, 'measure': 'ст.л'},
-        {'ingredient_name': 'Соевый соус', 'quantity': 60, 'measure': 'мл'}
-    ],
-    'Запеченный картофель': [
-        {'ingredient_name': 'Картофель', 'quantity': 1, 'measure': 'кг'},
-        {'ingredient_name': 'Чеснок', 'quantity': 3, 'measure': 'зубч'},
-        {'ingredient_name': 'Помидор', 'quantity': 4, 'measure': 'шт'},
-    ]
-}
+
+def making_cook_book():
+    with open('recipes.txt', 'r', encoding='utf-8') as f:
+        cook_book = {}
+        ingredient_list = []
+        x = []
+        count = 4
+
+        for line in f:
+            try:
+                line = int(line)
+                x.append(line)
+                count = len(x)
+            except:
+                continue
+
+    with open('recipes.txt', 'r', encoding='utf-8') as f:
+        while count > 0:
+            dish_name = f.readline().strip()
+            ingredient_count = int(f.readline().strip())
+
+            for _ in range(ingredient_count):
+                m = f.readline().strip().split('|')
+
+                ingredient_list += [{'ingredient_name': m[0], 'quantity': m[1], 'measure': m[2]}]
+
+            cook_book[dish_name] = ingredient_list
+            ingredient_list = []
+            f.readline().strip()
+            count -= 1
+    return cook_book
+
+# cook_book = {
+#     'Омлет': [
+#         {'ingredient_name': 'Яйцо', 'quantity': 2, 'measure': 'шт.'},
+#         {'ingredient_name': 'Молоко', 'quantity': 100, 'measure': 'мл'},
+#         {'ingredient_name': 'Помидор', 'quantity': 6, 'measure': 'шт'}
+#     ],
+#     'Утка по-пекински': [
+#         {'ingredient_name': 'Утка', 'quantity': 1, 'measure': 'шт'},
+#         {'ingredient_name': 'Вода', 'quantity': 2, 'measure': 'л'},
+#         {'ingredient_name': 'Мед', 'quantity': 3, 'measure': 'ст.л'},
+#         {'ingredient_name': 'Соевый соус', 'quantity': 60, 'measure': 'мл'}
+#     ],
+#     'Запеченный картофель': [
+#         {'ingredient_name': 'Картофель', 'quantity': 1, 'measure': 'кг'},
+#         {'ingredient_name': 'Чеснок', 'quantity': 3, 'measure': 'зубч'},
+#         {'ingredient_name': 'Помидор', 'quantity': 4, 'measure': 'шт'},
+#     ]
+# }
 
 
 def show_dish_list(dishes=None):
     if dishes is None:
-        dishes = cook_book
+        dishes = making_cook_book()
 
     dish_list = []
     new_dishes = {}
