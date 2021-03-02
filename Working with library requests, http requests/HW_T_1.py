198703ab1f2033c2baec25063da6315658b91a7f
theRMN <roman.vassev@gmail.com>
2021-03-03 02:56:23 +0600

    code optimization

--- Working with library requests, http requests/HW_T_1.py
+++ Working with library requests, http requests/HW_T_1.py
@@ -1,27 +1,32 @@
-from pprint import pprint
 import requests
-import json
 
-name_list = ['Hulk', 'Captain America', 'Thanos']
 
-intelligence_dict = {}
+def parsing_iq(names=None):
+    if names is None:
+        names = ['Hulk', 'Captain America', 'Thanos']
 
-x = []
+    intelligence_dict = {}
+    x = []
 
-for name in name_list:
-    url = 'https://superheroapi.com/api/2619421814940190/search/' + name
-    resp = requests.get(url)
-    intelligence = int(resp.json()['results'][0]['powerstats']['intelligence'])  # We get the IQ
-    hero_name = resp.json()['results'][0]['name']  # We get the name of superhero
-    intelligence_dict[hero_name] = intelligence
-    x.append(intelligence)
+    for name in names:
+        url = 'https://superheroapi.com/api/2619421814940190/search/' + name
+        resp = requests.get(url)
+        intelligence = int(resp.json()['results'][0]['powerstats']['intelligence'])  # We get the IQ
+        hero_name = resp.json()['results'][0]['name']  # We get the name of superhero
+        intelligence_dict[hero_name] = intelligence
+        x.append(intelligence)
 
-for i in intelligence_dict.items():
-    if i[1] == max(x):
-        print(i[0])
+    return {max(x): intelligence_dict}
 
-# print(resp.status_code)
-# print(resp.content)
-# pprint(resp.headers)
-# print(resp.text)
-# pprint(resp.json())
+
+def calculating_iq(parsing=None):
+    if parsing is None:
+        parsing = parsing_iq()
+
+    for i in parsing.items():
+        for o in i[1].items():
+            if o[1] == i[0]:
+                print(f'Superhero with maximum iq is: "{o[0]}"')
+
+
+calculating_iq()
