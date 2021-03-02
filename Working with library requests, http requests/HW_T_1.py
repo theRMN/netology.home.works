21ab4ce03b329e0036b2047c4cf47715a775aa3b
theRMN <roman.vassev@gmail.com>
2021-03-03 02:23:11 +0600

    iq comparison added

--- Working with library requests, http requests/HW_T_1.py
+++ Working with library requests, http requests/HW_T_1.py
@@ -6,15 +6,19 @@ name_list = ['Hulk', 'Captain America', 'Thanos']
 
 intelligence_dict = {}
 
+x = []
+
 for name in name_list:
     url = 'https://superheroapi.com/api/2619421814940190/search/' + name
     resp = requests.get(url)
-    x = resp.json()
-    intelligence = x['results'][0]['powerstats']['intelligence']  # Получаем показатель интелекта
-    hero_name = x['results'][0]['name']
+    intelligence = int(resp.json()['results'][0]['powerstats']['intelligence'])  # We get the IQ
+    hero_name = resp.json()['results'][0]['name']  # We get the name of superhero
     intelligence_dict[hero_name] = intelligence
+    x.append(intelligence)
 
-print(intelligence_dict)
+for i in intelligence_dict.items():
+    if i[1] == max(x):
+        print(i[0])
 
 # print(resp.status_code)
 # print(resp.content)
