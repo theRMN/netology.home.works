48e5e0da7d56dad3bfc752330a27c94d1a0f3e72
theRMN <roman.vassev@gmail.com>
2021-03-04 14:30:10 +0600

    add functions

--- Working with library requests, http requests/HW_T_3.py
+++ Working with library requests, http requests/HW_T_3.py
@@ -1,20 +1,34 @@
 import requests
 from datetime import datetime
 
-number = 0
-current_datetime = datetime.now().timestamp()
-while True:
-    number += 1
-    request = requests.get('https://api.stackexchange.com/2.2/questions',
-                           params={'todate': str(int(current_datetime)),
-                                   'fromdate': str(int(current_datetime) - 172800),
-                                   'order': 'desc',
-                                   'sort': 'creation',
-                                   'tagged': 'python',
-                                   'site': 'stackoverflow',
-                                   'pagesize': 100,
-                                   'page': 1
-                                   })
 
-    for i in request.json()['items']:
-        print(i['link'])
+def datetime_now():
+    current_datetime = datetime.now().timestamp()
+
+    return current_datetime
+
+
+def questions_parsing(current_datetime=None):
+    if current_datetime is None:
+        current_datetime = datetime_now()
+
+    number = 0
+
+    while True:
+        number += 1
+        request = requests.get('https://api.stackexchange.com/2.2/questions',
+                               params={'todate': str(int(current_datetime)),
+                                       'fromdate': str(int(current_datetime) - 172800),
+                                       'order': 'desc',
+                                       'sort': 'creation',
+                                       'tagged': 'python',
+                                       'site': 'stackoverflow',
+                                       'pagesize': 100,
+                                       'page': number
+                                       })
+
+        for i in request.json()['items']:
+            print(i['link'])
+
+
+questions_parsing()
